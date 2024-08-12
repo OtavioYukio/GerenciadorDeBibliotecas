3894621ab2a08c5986129174b87bf4de1c0e629f
Otavio Yukio <yukiootavio@gmail.com>
2024-08-12 16:27:13 -0300

    Configuradas as classes Book e User

---
+++ entities/User.java
@@ -0,0 +1,36 @@
+package entities;
+
+import java.util.List;
+
+public class User {
+    private String name;
+    private Integer id;
+    private List emprestimos;
+
+    User(String name, Integer id, List emprestimos) {
+        this.name = name;
+        this.id = id;
+        this.emprestimos = emprestimos;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public List getEmprestimos() {
+        return emprestimos;
+    }
+    public void setEmprestimos(List emprestimos) {
+        this.emprestimos = emprestimos;
+    }
+}
