b57cf402a372f1980e503775c6f8fab0ded03659
Otavio Yukio <yukiootavio@gmail.com>
2024-08-12 17:23:01 -0300

    Funções principais programadas, importadas bibliotecas na classe Program

--- func/Biblioteca.java
+++ func/Biblioteca.java
@@ -1,5 +1,44 @@
 package func;
 
+import entities.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Biblioteca {
+
+    public static void cadastrarLivro(Book newBook) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o titulo: ");
+        newBook.setTitle(sc.nextLine());
+        System.out.print("Digite o nome do autor: ");
+        newBook.setAuthor(sc.nextLine());
+        System.out.print("Digite o genero: ");
+        newBook.setGenre(sc.nextLine());
+    }
+
+    public static void editarLivro(Book editBook) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("O que deseja alterar?\n1 - Titulo\n2 - Autor\n3 - Genero");
+        int option = sc.nextInt();
+        sc.nextLine();
+
+        if (option == 1) {
+            editBook.setTitle(sc.nextLine());
+        }
+        else if (option == 2) {
+            editBook.setTitle(sc.nextLine());
+        }
+        else if (option == 3) {
+            editBook.setGenre(sc.nextLine());
+        }
+        else {
+            System.out.println("Opção invalida, tente novamente");
+            editarLivro(editBook);
+        }
+    }
     
 }
--- project/Program.java
+++ project/Program.java
@@ -1,5 +1,16 @@
 package project;
 
+import entities.Book;
+import entities.User;
+import func.Biblioteca;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Program {
-    
+
+    public static void main(String[] args) {
+        
+    }
+
 }
